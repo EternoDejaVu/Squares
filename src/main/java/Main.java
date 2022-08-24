8779be5d20750611e3e69d6b176002d65e42f85f
EternoDejaVu <105560004+EternoDejaVu@users.noreply.github.com>
2022-08-24 19:59:39 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import ru.netology.sqr.Squares.services.SQRService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SQRService service = new SQRService();
+        System.out.println(service.calcSQRT(10, 99));
+    }
+}
+
+
---
+++ src/test/java/ru/netology/sqr/Squares/services/SQRServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr.Squares.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class SQRServiceTest {
+
+    @Test
+    public void accountCalc16() {
+        SQRService service = new SQRService();
+
+        int expected = 6;
+        int actual = service.calcSQRT(10, 99);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
